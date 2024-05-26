4b1defddb176a8298e256971678844795230ef90
RaissaParente <raissabparente@gmail.com>
2024-05-26 13:38:02 -0300

    created forum card

--- hack/Utils/ForumPostCard.swift
+++ hack/Utils/ForumPostCard.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ForumPostCard: View {
+    
+    var textBody: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "house")
+                    .frame(width: 35, height: 35)
+                    .clipShape(Circle())
+                    .background {
+                        Circle()
+                            .stroke(.black)
+                    }
+                VStack {
+                   Text("Username")
+                        .font(.system(size: 12))
+                    
+                    Text("Mentora")
+                        .font(.system(size: 8))
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 10)
+                        .background(Capsule().foregroundStyle(.yellow))
+                }
+            }
+            
+            Image(systemName: "house")
+               .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)                 .frame(maxHeight: 220)
+                .background(.yellow)
+            
+            Text(textBody)
+                .font(.system(size: 12))
+            
+            Text("data")
+                .frame(width: .infinity, alignment: .trailing)
+        }
+        .padding()
+        .background(.gray)
     }
 }
 
 #Preview {
-    ForumPostCard()
+    ForumPostCard(textBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi. Vivamus vitae massa sed ligula commodo pulvinar.")
 }
