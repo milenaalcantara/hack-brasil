298ef769f3701b79891cf1fbf4e83018ec63ce9b
Milena Alcantara <milimaalc.dev@gmail.com>
2024-05-26 07:55:41 -0300

    Initial Commit

---
+++ hack/hackApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackApp.swift
+//  hack
+//
+//  Created by Milena Alcântara on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct hackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
