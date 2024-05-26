7e12b12eae0517e6cce84abf1e04ccf1e127719e
RaissaParente <raissabparente@gmail.com>
2024-05-26 13:52:54 -0300

    added buttons and comment to forum post

--- hack/Utils/ForumPostCard.swift
+++ hack/Utils/ForumPostCard.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ForumPostCard: View {
     
     var textBody: String = ""
+    @State var seeComments = false
+    @State var newComment: String = ""
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -38,18 +40,49 @@ struct ForumPostCard: View {
                 .scaledToFit()
                 .frame(maxWidth: .infinity)                 .frame(maxHeight: 220)
                 .background(.yellow)
+                .padding(.vertical)
             
             Text(textBody)
                 .font(.system(size: 12))
             
             Text("data")
-                .frame(width: .infinity, alignment: .trailing)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            HStack {
+                Button {
+                    //
+                } label: {
+                    Text("like")
+                }
+                
+                Button {
+                    seeComments.toggle()
+                } label: {
+                    Text("comment")
+                }
+            }
+            
+            HStack {
+                TextField("Escreva um comentário", text: $newComment)
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "camera")
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Enviar")
+                }
+                .buttonStyle(.borderedProminent)
+            }
         }
         .padding()
-        .background(.gray)
+        .background(.gray.opacity(0.5))
     }
 }
 
 #Preview {
-    ForumPostCard(textBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi. Vivamus vitae massa sed ligula commodo pulvinar.")
+    ForumPostCard(textBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi. Vivamus vitae massa sed ligula commodo pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisi. Vivamus vitae massa sed ligula commodo pulvinar.")
 }
