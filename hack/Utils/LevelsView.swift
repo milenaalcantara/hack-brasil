09f90add4f82693b4ceba6a13701f880989408ba
RaissaParente <raissabparente@gmail.com>
2024-05-26 13:37:22 -0300

    levels intergrated into forum

--- hack/Utils/LevelsView.swift
+++ hack/Utils/LevelsView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct LevelsView: View {
+    
+    var levels: [Int] = [1,2,3,4,5]
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Level up")
+                .padding()
+            
+            ZStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(Color.gray)
+                    .padding(.bottom, 22)
+                
+                HStack(spacing: 30) {
+                    ForEach(levels, id: \.self) { level in
+                        
+                        VStack {
+                            
+                            Image(systemName: "house")
+                                .frame(width: 45, height: 45)
+                                .clipShape(Circle())
+                                .background {
+                                    Circle()
+                                        .stroke(.black)
+                                }
+                            
+                            Text("texto")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
